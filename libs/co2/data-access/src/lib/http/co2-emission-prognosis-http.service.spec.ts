938eec7fbcd602ea3590bd27a1a248b0e23fa6bc
Christian Russ <russ.c@eplan.de>
2021-07-09 06:30:57 +0200

    feat: add CO2 Emission Prognisis Http client

---
+++ libs/co2/data-access/src/lib/http/co2-emission-prognosis-http.service.spec.ts
@@ -0,0 +1,16 @@
+import { Co2EmissionPrognosisHttp } from './co2-emission-prognosis-http.service';
+import {TestBed} from '@angular/core/testing'; // Configures and initializes environment for unit testing
+import {Observable }  from 'rxjs';
+
+describe(Co2EmissionPrognosisHttp.name, () => {
+    let sut: Co2EmissionPrognosisHttp;
+    
+    beforeEach(() => {
+        sut = TestBed.inject(Co2EmissionPrognosisHttp)
+    });
+    
+    it('resolves an array', async () => {
+        const result = await sut.get().toPromise(); // will be removed with RxJS 8
+        expect(result).toEqual([]);
+    });
+})
---
+++ libs/co2/data-access/src/lib/http/co2-emission-prognosis-http.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+// see https://www.energidataservice.dk/tso-electricity/co2emisprog DATA API dialog
+
+const url = 'https://api.energidataservice.dk/datastore_search_sql'
+// https://api.energidataservice.dk/datastore_search_sql?sql=SELECT * from "co2emisprog" LIMIT 5
+
+export interface Co2EmissionPrognosisRecord {
+    readonly minutes5UTC: Date;
+    readonly co2Emission: number;
+    readonly priceArea: 'DK1' | 'DK2';
+}
+
+/*
+    Represents a stateless services  
+    ... we provided it in the root;
+    ... we have one instance for the whole application;
+    ... this is an internal service and therefore not provided in the public API file.
+ */
+@Injectable({
+    providedIn: 'root'
+})
+export class Co2EmissionPrognosisHttp {
+    get(): Observable<readonly Co2EmissionPrognosisRecord[]> {
+        return of([]);
+    }
+}
