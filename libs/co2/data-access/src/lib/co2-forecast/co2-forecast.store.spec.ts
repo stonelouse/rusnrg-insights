4e76df886fe8b3aad2f5569b8d2b6e509713e8dc
Christian Russ <russ.c@eplan.de>
2021-07-12 06:20:49 +0200

    feat: add record$ selector to the store

--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import {Co2ForecastStore} from './co2-forecast.store'
+import {Co2ForecastStore} from './co2-forecast.store';
+import {first} from 'rxjs/operators'
+
 
 describe(Co2ForecastStore.name, () => {
     function setup() {
@@ -21,4 +23,14 @@ describe(Co2ForecastStore.name, () => {
         console.log({store});
         expect(store).not.toBeNull();
     });
+
+    describe('records$', () => {
+        it('it initially emits 0 records', async () => {
+            const { store } = setup();
+            const records = await store.records.pipe(
+                first()
+            ).toPromise();
+            expect(records).toEqual([]);
+        })
+    })
 });
--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'; // Injectable decorator FACTORY
 import { ComponentStore } from '@ngrx/component-store';
 import { Co2EmissionPrognosisRecords } from './co2-emission-prognosis-record';
+import { Observable } from 'rxjs'
 
 export interface Co2ForecastState {
     readonly records: Co2EmissionPrognosisRecords;
@@ -9,6 +10,12 @@ export interface Co2ForecastState {
 /* TODO: we have to find out the appropriate store eventually */
 @Injectable()  // we provide a parameter to the factory and then it returns an decorator.
 export class Co2ForecastStore extends ComponentStore<Co2ForecastState>{
+
+    // ngrx's 'select' method creates an selector
+    records: Observable<Co2EmissionPrognosisRecords> = this.select(
+        state => state.records
+    )
+
     constructor() {
         // Its important to initialize store's state!  */
         super(initialState);
@@ -16,5 +23,8 @@ export class Co2ForecastStore extends ComponentStore<Co2ForecastState>{
 }
 
 const initialState: Co2ForecastState = {
-    records: [],
+    records: [
+        // To check if we test the right thing ...
+        // {co2Emission: 180, minutes5UTC: new Date('2021-05-19T22:15:00+02:00'), priceArea:'DK1'}
+    ],
 }
